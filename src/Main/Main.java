d3e7c81318c6ae563ce45c3277065c1460d6ebfa
SeenLex <draghicidenis@yahoo.com>
2023-04-13 13:29:36 +0300

    Added volume control button.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,27 +1,76 @@
 package Main;
 
 import Screens.MainMenu.MainMenuMain;
+import Screens.SelectedFunction.SequentialRead.SequentialReadMain;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.*;
 import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
-
+import javafx.geometry.Pos;
 import java.io.File;
+
+import static Shared.ButtonsHelper.*;
 
 public class Main extends Application {
 
-    public void playMainMenuMusic(){
+    private AudioClip audioClip;
 
+    public void playMainMenuMusic(){
         File file = new File("SoundFiles/ScoobyDooMusic.wav");
-        AudioClip audioClip = new AudioClip(file.toURI().toString());
+        audioClip = new AudioClip(file.toURI().toString());
         audioClip.setVolume(0.3);
         audioClip.play();
+    }
+
+    public void volumeButton(StackPane root, BorderPane pane){
+
+
+        double xCoords = 1150;
+        double yCoords = -600;
+
+        double xScale = 2600;
+        double yScale = 1500;
+
+        EventHandler<ActionEvent> event = e -> {
+            playSoundClick("ButtonCloudPressed.wav", 3);Slider volumeSlider = new Slider(0, 1, 0.3);
+            volumeSlider.setShowTickMarks(true);
+            volumeSlider.setShowTickLabels(true);
+            volumeSlider.setMajorTickUnit(0.1);
+            volumeSlider.setMinorTickCount(0);
+            volumeSlider.setSnapToTicks(true);
+
+            Label volumeLabel = new Label("Volume: 0.3");
+            volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+                double volume = newValue.doubleValue();
+                audioClip.setVolume(volume);
+                volumeLabel.setText("Volume: " + String.format("%.1f", volume));
+            });
+
+            StackPane volumePane = new StackPane(volumeSlider, volumeLabel);
+            volumePane.setAlignment(Pos.TOP_RIGHT);
+
+            pane.setTop(volumePane);
+
+        };
+
+         Button b = buttonBuilder("sequentialRead",  root, event, pane);
+         //Replace with a Slider
+         scaleButton(b,root,xScale,yScale, xCoords, yCoords);
 
     }
 
+    @Override
+    public void stop() {
+        audioClip.stop();
+    }
     @Override
     public void start(Stage primaryStage){
         StackPane root = new StackPane();
@@ -36,6 +85,8 @@ public class Main extends Application {
         primaryStage.show();
 
         playMainMenuMusic();
+
+        volumeButton(root, pane);
     }
 
     public static void main(String[] args) {
