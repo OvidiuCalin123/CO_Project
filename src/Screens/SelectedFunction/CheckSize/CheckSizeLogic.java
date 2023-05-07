091c9b9171638a28794b318460dd1a06ad6ea42f
Calin Ovidiu <calinovidiu15@yahoo.ro>
2023-05-07 17:27:25 +0300

    Removed dependency for CheckSizeLogic.java file

--- src/Screens/SelectedFunction/CheckSize/CheckSizeLogic.java
+++ src/Screens/SelectedFunction/CheckSize/CheckSizeLogic.java
@@ -1,10 +1,7 @@
 package Screens.SelectedFunction.CheckSize;
 
 import Screens.SelectedFunction.SelectedFunctionLogicHandle;
-
-import org.apache.commons.io.FileUtils;
 import java.io.File;
-import java.io.IOException;
 
 public class CheckSizeLogic implements SelectedFunctionLogicHandle {
 
@@ -21,25 +18,18 @@ public class CheckSizeLogic implements SelectedFunctionLogicHandle {
     }
 
     public void run(){
+
         File hardDrive = new File("C:\\");
+
         long startTime = System.currentTimeMillis();
         long sizeInBytes = getHardDriveSizeInBytes(hardDrive);
         long sizeInGB = convertBytesToGB(sizeInBytes);
         long endTime = System.currentTimeMillis();
-        if (hardDrive.exists() && hardDrive.isDirectory()) {
-            try {
-                long realSize = FileUtils.sizeOfDirectory(hardDrive);
-                long realSizeInGB = convertBytesToGB(realSize);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("Drive not found.");
-        }
-        speed = realSizeInGB;
-        sizeInBytes=realSize;
+
+        speed = sizeInGB;
         time = endTime - startTime;
         isCompleted = true;
+
         System.out.println("Size of hard drive in bytes: " + sizeInBytes);
         System.out.println("Size of hard drive in GB: " + sizeInGB);
     }
